3e2663779b05fa9080b272b0e1c1e40be89c6da0
KauanC. <kauanderengoski21@gmail.com>
2026-05-20 19:37:06 -0300

    jeito errado

--- forntand/src/App.tsx
+++ forntand/src/App.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
 
 import './App.css'
 
 function App() { //java script e type
-  const [cont  , setContador ] = useState <number>(0);
-
+ 
  
 
   return (
     // html e css
     <>
-    <div>
-      Kauan C - 
-
-      { cont }
-    </div>
-
-    <button onClick={()=>  setContador(cont +1000) } >ALTERAR IDADE</button>
-    <button onClick={()=>  setContador(cont -1000) } >ALTERAR IDADE</button>
-    <div>
-      HELLO_WORLD_I_LIKE
-    </div>
+   KAUAN
+  
    </>   
   ) ///trablhar css Fim
 }
