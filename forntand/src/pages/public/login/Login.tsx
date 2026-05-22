436d8fe26ce41f269726eea6673dca755e00aa81
KauanC. <kauanderengoski21@gmail.com>
2026-05-21 22:25:28 -0300

    trabalho 30% feito

--- forntand/src/pages/public/login/Login.tsx
+++ forntand/src/pages/public/login/Login.tsx
@@ -1,20 +1,32 @@
-import style from './Login.module.css'
-function Login(){ //TYPESCRIPT
 
-   return ( // HTML
-<>
-<h1>acessar sua conta</h1>
-<input className={style.input} type="email" placeholder="email"></input>
-
-<input className={style.input} type="password" placeholder="senha"></input>
-<button className={style.button}>acessar</button>
-
- 
-</>
+import style from "./Login.module.css";
+function Login() {
+  //TYPESCRIPT
 
 
+  return (
+    // HTML
+    <div className={style.conteiner}>
+      <div className={style.acesso}>
+        <h1>Bem-vindo!!</h1>
+        <p>faça seu login</p>
+    
+      <input className={style.input} type="email" placeholder="email"></input>
 
+      <input
+        className={style.input}
+        type="password"
+        placeholder="senha"
+      ></input>
+
+      <a className={style.linkacc} href="https://earth3dmap.com/">
+        esqueceu sua senha? clique aqui:
+      </a>
+    
+      <button  className={style.button}>acessar</button>
+    </div>
+</div>
   );
 }
 
- export default Login;
+export default Login;
