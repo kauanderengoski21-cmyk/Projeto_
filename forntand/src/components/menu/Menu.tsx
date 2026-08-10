0067a3ff4b0f70c8c0e872803b9806ecbdac4967
KauanC. <kauanderengoski21@gmail.com>
2026-08-10 20:34:58 -0300

    arrumando a parte do front end da parte dos menus de cadastro e login entre outros

--- forntand/src/components/menu/Menu.tsx
+++ forntand/src/components/menu/Menu.tsx
@@ -1,39 +1,31 @@
 import { NavLink } from "react-router-dom";
-
-
-interface  MenuInstrucao {
- label: string,
- path: string
+import style  from  "./Menu.module.css";
+interface MenuInstrucao {
+  label: string;
+  path: string;
 }
 
-
-const menuInstrucao:MenuInstrucao[] = [
-    {label: "inicio", path:"/principal"},
-    {label: "Cadastro", path:"/cadastro"},
-    {label: "login", path:"/login"},
+const menuInstrucao: MenuInstrucao[] = [
+  { label: "Inicio", path: "/principal" },
+  { label: "Cadastro", path: "/cadastro" },
+  { label: "Login", path: "/login" },
+  { label: "configuracao", path: ""}
 ];
 
-const Menu: React.FC = ()=>{
-
-    return(
-        <>
-        <nav>
-            <ul>
-        {
-        menuInstrucao.map (x =>(
-         <li>
-             <NavLink to={x.path}>
-                {x.label}
-             </NavLink>
-         </li>   
-        )
-        )
-        }
-            </ul>
-        </nav>
-        </>
-    );
-}
-
+const Menu: React.FC = () => {
+  return (
+    <nav className={style.menu}>
+      <ul className={style.menulist}>
+        {menuInstrucao.map((item) => (
+          <li className={style.menuprodut} key={item.path}>
+            <NavLink className={style.menulink} to={item.path}>
+              {item.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
